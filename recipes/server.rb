b1aaae37e198199eba395fd0863c456c70c536c2
Jesse Nelson <spheromak@gmail.com>
2013-02-07 01:24:35 -0800

    ...

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,7 +7,7 @@ include_recipe "vagrant-post"
 validator =  "/etc/chef/validation.pem" 
 knife_args = "--defaults -s http://localhost:#{node[:vagrant][:chef_server][:port]}"
 
-if  node[:vagrant][:chef_server][:version] == 10 
+if 11 == node[:vagrant][:chef_server][:version] 
   validator =  "/etc/chef-server/chef-validator.pem"
   knife_args = " -s https://localhost:#{node[:vagrant][:chef_server][:port]} --admin-client-key /etc/chef-server/admin.pem --admin-client-name admin --validation-client-name chef-validator  --validation-key=/etc/chef-server/chef-validator.pem"
 end
