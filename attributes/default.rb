e6d31439465cc6542bfcec5707f090688d19fe92
Jesse Nelson <spheromak@gmail.com>
2013-02-18 07:01:51 -0800

    move srver url to server.vm

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:vagrant][:knife_dir] = "/vagrant/.chef"
-default[:vagrant][:chef_server][:url] = "http://localhost:8000"
-default[:vagrant][:chef_server][:version] = 11
+default[:vagrant][:chef_server][:url] = "http://server.vm:8000"
+default[:vagrant][:chef_server][:version] = 10
 
