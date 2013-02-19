5dd9de5bea2b62414315b39ee74b4e6a37e17cb1
Jesse Nelson <spheromak@gmail.com>
2013-02-18 17:48:32 -0800

    use chef11 as default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:vagrant][:knife_dir] = "/vagrant/.chef"
-default[:vagrant][:chef_server][:url] = "http://server.vm:8000"
-default[:vagrant][:chef_server][:version] = 10
+default[:vagrant][:chef_server][:url] = "https://server.vm:443"
+default[:vagrant][:chef_server][:version] = 11
 
